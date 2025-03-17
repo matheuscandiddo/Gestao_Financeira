a615bacbec19c336fa515bb0f250b72227764466
Matheus Cândido <matheuscandido.corporativo@gmail.com>
2025-03-17 15:16:42 -0300

    V_1

--- components/dashboards.py
+++ components/dashboards.py
@@ -10,11 +10,64 @@ import calendar
 from globals import *
 from app import app
 
+card_icon = {
+    "color":"white",
+    "textAlign":"center",
+    "fontSize": 30,
+    "margin": "auto"
+}
 
 
 # =========  Layout  =========== #
 layout = dbc.Col([
-    html.H5('Dashboards')
+    html.H5('Dashboards'),
+    dbc.Row([
+
+        # Card de Saldo
+        dbc.Col([
+            dbc.CardGroup([
+                dbc.Card([
+                    html.Legend('Saldo'),
+                    html.H5('R$ 5000', id='p-saldo-dashboard', style={})
+                ], style={'padding-left':'20px', 'padding-top':'10px'}),
+                dbc.Card(
+                    html.Div(className='fa fa-university', style=card_icon),
+                    color='warning',
+                    style={'maxWidth': 75, 'height': 100, 'margin-left': '-10px'}
+                )
+            ])
+        ], width=4),
+
+        # Card de Receita
+                dbc.Col([
+            dbc.CardGroup([
+                dbc.Card([
+                    html.Legend('Saldo'),
+                    html.H5('R$ 5000', id='p-saldo-dashboard', style={})
+                ], style={'padding-left':'20px', 'padding-top':'10px'}),
+                dbc.Card(
+                    html.Div(className='fa fa-university', style=card_icon),
+                    color='warning',
+                    style={'maxWidth': 75, 'height': 100, 'margin-left': '-10px'}
+                )
+            ])
+        ], width=4),
+
+        # Card de Despesa
+                dbc.Col([
+            dbc.CardGroup([
+                dbc.Card([
+                    html.Legend('Saldo'),
+                    html.H5('R$ 5000', id='p-saldo-dashboard', style={})
+                ], style={'padding-left':'20px', 'padding-top':'10px'}),
+                dbc.Card(
+                    html.Div(className='fa fa-university', style=card_icon),
+                    color='warning',
+                    style={'maxWidth': 75, 'height': 100, 'margin-left': '-10px'}
+                )
+            ])
+        ], width=4),
+    ])
        
     ])
 

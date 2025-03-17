fa3516f180ace71a0342c9622e86f810a94fb63b
Matheus Cândido <matheuscandido.corporativo@gmail.com>
2025-03-17 11:23:09 -0300

    V_1

--- myindex.py
+++ myindex.py
@@ -6,8 +6,7 @@ import pandas as pd
 import plotly.express as px
 
 from app import *
-
-
+from components import sidebar, dashboards, extratos
 
 
 # =========  Layout  =========== #
@@ -15,15 +14,26 @@ content = html.Div(id="page-content")
 
 
 app.layout = dbc.Container(children=[
-
-
-
-
+    dbc.Row([
+        dbc.Col([
+            dcc.Location(id='url'),
+            sidebar.layout
+        ], md=2),
+        dbc.Col([
+            content
+        ], md=10)
+    ])
 
 ], fluid=True,)
 
 
-
+@app.callback(Output('page-content','children'), [Input('url','pathname')])
+def render_page(pathname):
+    if pathname == '/' or pathname == '/dashboards':
+        return dashboards.layout
+    
+    if pathname == '/extratos':
+        return extratos.layout
 
 if __name__ == '__main__':
     app.run_server(port=8051, debug=True)
